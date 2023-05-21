deb4b8203c6d5693208cbd2107d6e5e85abf0c82
KleytonFSantos <akatsukipb12@gmail.com>
2023-05-21 10:56:15 -0300

    deploy conf

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,6 +5,7 @@ composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
+php artisan cache:clear
 
 echo "Caching routes..."
 php artisan route:cache
@@ -14,6 +15,8 @@ php artisan migrate --force
 
 echo "Setting cache permissions..."
 chmod -R 777 /var/www/html/storage/framework/cache
+chmod -R 777 /var/www/html/storage/framework
+chmod -R 775 storage/framework
 chmod -R 775 storage/framework/cache
 chmod -R 775 bootstrap/cache
 chmod -R 777 /var/www/html/bootstrap/cache
