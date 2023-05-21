be7464bccccfca9a292589ade1d52ce6b93a053c
KleytonFSantos <akatsukipb12@gmail.com>
2023-05-21 09:11:12 -0300

    [FEAT] - adicionando cache no feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use App\Http\Resources\FeedResource;
 use App\Models\Post;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class FeedController extends Controller
 {
     public function __invoke(Post $model): AnonymousResourceCollection
     {
-        $posts = $model->allPosts();
+        if (Cache::has('posts')) {
+            $posts = Cache::get('posts');
+        } else {
+            $posts = $model->allPosts();
+
+            Cache::put('posts', $posts, 60 * 60);
+        }
 
         return FeedResource::collection($posts);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -17,11 +18,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
@@ -33,36 +29,26 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    public function userProfile()
+    public function userProfile(): HasOne
     {
        return $this->hasOne(UserProfile::class);
     }
 
-    public function song()
+    public function song(): HasMany
     {
        return $this->hasMany(Song::class);
     }
 
-    public function video()
+    public function video(): HasMany
     {
        return $this->hasMany(Video::class);
     }
@@ -74,6 +60,6 @@ class User extends Authenticatable
 
     public function findUserByEmail($email): User
     {
-        $this->where('email', $email)->first();
+        return $this->where('email', $email)->first();
     }
 }
