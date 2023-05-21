5fa29e7a326884f246be27c0cd92ea9be8732942
KleytonFSantos <akatsukipb12@gmail.com>
2023-05-21 11:00:37 -0300

    deploy conf

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,15 +3,23 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+# shellcheck disable=SC2164
+cd storage/
+mkdir -p framework/{sessions,views,cache}
+chmod -R 775 framework
+
 echo "Caching config..."
-php artisan config:cache
 php artisan cache:clear
+php artisan config:clear
+php artisan view:clea
 
 echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+
 
 echo "Setting cache permissions..."
 chmod -R 777 /var/www/html/storage/framework/cache
