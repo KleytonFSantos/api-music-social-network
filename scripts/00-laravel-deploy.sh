03266c346b89481d33731d342700008f99ff0404
KleytonFSantos <akatsukipb12@gmail.com>
2023-05-21 10:45:00 -0300

    deploy conf

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -14,4 +14,6 @@ php artisan migrate --force
 
 echo "Setting cache permissions..."
 chmod -R 777 /var/www/html/storage/framework/cache
+chmod -R 775 storage/framework/cache
+chmod -R 775 bootstrap/cache
 chmod -R 777 /var/www/html/bootstrap/cache
